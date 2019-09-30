e6888109862da2a06128694b9b35ff90aea49730
Boyko <boikoneov@gmail.com>
2019-09-30 17:22:33 +0300

    Added reverse complement button functionality, some design changes

--- IBERPrime/Form1.Designer.cs
+++ IBERPrime/Form1.Designer.cs
@@ -37,11 +37,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.OutputMessages = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // FindPrimers
             // 
-            this.FindPrimers.Location = new System.Drawing.Point(1136, 486);
+            this.FindPrimers.Location = new System.Drawing.Point(1057, 420);
             this.FindPrimers.Name = "FindPrimers";
             this.FindPrimers.Size = new System.Drawing.Size(75, 23);
             this.FindPrimers.TabIndex = 0;
@@ -51,26 +52,29 @@
             // richTextBox1
             // 
             this.richTextBox1.Location = new System.Drawing.Point(12, 34);
+            this.richTextBox1.MaxLength = 20000;
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(1201, 163);
+            this.richTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.richTextBox1.Size = new System.Drawing.Size(1120, 124);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(12, 254);
+            this.richTextBox2.Location = new System.Drawing.Point(12, 198);
+            this.richTextBox2.MaxLength = 20000;
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(1201, 163);
+            this.richTextBox2.Size = new System.Drawing.Size(1120, 123);
             this.richTextBox2.TabIndex = 2;
             this.richTextBox2.Text = "";
             this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
             // 
             // richTextBox3
             // 
-            this.richTextBox3.Location = new System.Drawing.Point(12, 528);
+            this.richTextBox3.Location = new System.Drawing.Point(12, 448);
             this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(1201, 163);
+            this.richTextBox3.Size = new System.Drawing.Size(1120, 163);
             this.richTextBox3.TabIndex = 3;
             this.richTextBox3.Text = "";
             // 
@@ -88,7 +92,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(9, 235);
+            this.label2.Location = new System.Drawing.Point(12, 176);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(135, 16);
             this.label2.TabIndex = 5;
@@ -96,37 +100,51 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1066, 5);
+            this.button1.Location = new System.Drawing.Point(987, 5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(145, 23);
             this.button1.TabIndex = 6;
             this.button1.Text = "Reverse Complement";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1066, 225);
+            this.button2.Location = new System.Drawing.Point(998, 196);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(145, 23);
             this.button2.TabIndex = 7;
             this.button2.Text = "Reverse complement";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(12, 512);
+            this.label3.Location = new System.Drawing.Point(12, 420);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(64, 16);
             this.label3.TabIndex = 8;
             this.label3.Text = "Results:";
             // 
+            // OutputMessages
+            // 
+            this.OutputMessages.AutoSize = true;
+            this.OutputMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputMessages.ForeColor = System.Drawing.Color.OrangeRed;
+            this.OutputMessages.Location = new System.Drawing.Point(15, 338);
+            this.OutputMessages.Name = "OutputMessages";
+            this.OutputMessages.Size = new System.Drawing.Size(17, 16);
+            this.OutputMessages.TabIndex = 9;
+            this.OutputMessages.Text = "...";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1269, 703);
+            this.ClientSize = new System.Drawing.Size(1155, 687);
+            this.Controls.Add(this.OutputMessages);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -154,5 +172,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label OutputMessages;
     }
 }
--- IBERPrime/Form1.cs
+++ IBERPrime/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text;
 
 namespace IBERPrime
 {
@@ -19,17 +20,112 @@ namespace IBERPrime
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
-                ProcessAllLines(this.richTextBox1);
-        }
-
-        private void ProcessAllLines(RichTextBox richTextBox)
-        {
-            throw new NotImplementedException();
+            ProcessAllLines(this.richTextBox1);
         }
 
         private void richTextBox2_TextChanged(object sender, EventArgs e)
         {
             ProcessAllLines(this.richTextBox2);
+        }
+
+        /// <summary>
+        /// Checks input for validity - currently valid letters are A T G C, all others are change to 'N'
+        /// </summary>
+        /// <param name="richTextBox"></param>
+        private void ProcessAllLines(RichTextBox richTextBox)
+        {
+            bool nonStandardLettersFound = false;
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char ch in richTextBox.Text.ToUpper())
+            {
+                if (ch == 'A' || ch == 'C' || ch == 'G' || ch == 'T')
+                {
+                    sb.Append(ch);
+                }
+                else
+                {
+                    nonStandardLettersFound = true;
+                    sb.Append('N');
+                }
+            }
+
+            richTextBox.Text = sb.ToString();
+
+            // returns cursor to the end of text
+            richTextBox.Select(richTextBox.Text.Length, 0);
+
+            if (nonStandardLettersFound)
+            {
+                OutputMessages.Text = "Non standard characters found. Non standard characters are substituted to 'N'.";
+            }
+        }
+
+
+        /// <summary>
+        /// Reverse complement, box 1 (Sequence 1)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ReverseComplement(richTextBox1);
+        }
+
+        /// <summary>
+        /// Reverse complement for box 2 (sequence 2)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReverseComplement(richTextBox2);
+        }
+
+        private void ReverseComplement(RichTextBox richTextBox)
+        {
+            char[] charArray = richTextBox.Text.ToCharArray();
+            Array.Reverse(charArray);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char ch in charArray)
+            {
+                switch (ch)
+                {
+                    case 'A':
+                        {
+                            sb.Append('T');
+                            break;
+                        }
+
+                    case 'T':
+                        {
+                            sb.Append('A');
+                            break;
+                        }
+
+                    case 'C':
+                        {
+                            sb.Append('G');
+                            break;
+                        }
+
+                    case 'G':
+                        {
+                            sb.Append('C');
+                            break;
+                        }
+
+                    default:
+                        {
+                            sb.Append('N');
+                            break;
+                        }
+                }
+
+                richTextBox.Text = sb.ToString();
+                richTextBox.Select(richTextBox.Text.Length, 0);
+            }
         }
     }
 }
